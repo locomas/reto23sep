9f5a3d2ed979a3cbf2ff8703024bdac2ca2a1491
209 <209@academicos.cesde.edu.co>
2022-09-23 20:53:23 -0500

    Trate de hacer la funcion

--- temperaturas.py
+++ temperaturas.py
@@ -1,5 +1,12 @@
 # El valle del aburrra afronta altas temperaturas. Crea una funcion que permita calc la temp media de la tierra, a partir de recibir 20 datos diarios de temp
 
 numeros = [1,5,1,2,5,2,6,3,6,3,2,6,3,4,6,7,3,4,3,2]
+media=0
 def calcularTemp(numeros):
-    print("se crea funcion")
+    for numero in numeros:
+        suma+=numero
+        media=suma/20
+        media=round(media,2)#redondeamos a 2 decimales
+print('La temperatura media de la tierra es de '+str(media))
+
+calcularTemp(numeros)
