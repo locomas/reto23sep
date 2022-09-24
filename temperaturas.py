b7cc32319eac6cba24c17a8be79306adadce7cbf
sebastian <locomas2013@hotmail.com>
2022-09-23 20:43:50 -0500

    se crea funcion

--- temperaturas.py
+++ temperaturas.py
@@ -1,1 +1,5 @@
 # El valle del aburrra afronta altas temperaturas. Crea una funcion que permita calc la temp media de la tierra, a partir de recibir 20 datos diarios de temp
+
+numeros = [1,5,1,2,5,2,6,3,6,3,2,6,3,4,6,7,3,4,3,2]
+def calcularTemp(numeros):
+    print("se crea funcion")
